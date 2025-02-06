470fdaa581b079c3afb07fd0162c42a01eda346f
Camila Pires Levi <camilaplevi@gmail.com>
2025-02-05 21:36:52 -0300

    feat/Adição componente Footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './Header/Header';
 import Sidebar from './Sidebar/Sidebar';
 import Main from './Main/main';
+import Footer from './Footer/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Sidebar/>
     <Header/>
     <Main/>
+    <Footer/>
     </div>
     
   );
